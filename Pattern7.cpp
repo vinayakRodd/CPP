deb6e00fcc3ab722029b50be3f0a8fee1d259718
Vinayak Rodd <150567680+vinayakRodd@users.noreply.github.com>
2024-05-13 19:20:27 +0530

    Create Pattern7.cpp

---
+++ Pattern7.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace  std;
+
+
+void print1(int n){
+    
+    int stars=1,spaces;
+
+    for(int i=1; i<=n; i++){
+
+        spaces  = i;
+        
+        for(int k=1; k<=spaces; k++)
+            cout<<" ";
+
+        for(int j=n; j>=stars; j--)
+            cout<<"* ";
+
+        cout<<endl;
+        
+        stars++;
+    }
+
+}
+
+int main(){
+
+    int n;
+
+    cout<<"Enter the number of lines: ";
+    cin>>n;
+
+    print1(n);
+    
+
+
+
+}

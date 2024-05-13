59236af9f6846f15ded4c0135f85973dfe175066
Vinayak Rodd <150567680+vinayakRodd@users.noreply.github.com>
2024-05-13 18:46:14 +0530

    Create Pattern1.cpp

---
+++ Pattern1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace  std;
+
+
+void print1(int n){
+    
+    int stars;
+
+    for(int i=n; i>=1; i--){
+
+        stars  = i;
+
+        for(int j=stars; j>=1; j--)
+            cout<<"* ";
+
+        cout<<endl;
+    }
+
+}
+
+int main(){
+
+    int n;
+
+    cin>>n;
+
+    print1(n);
+    
+
+
+
+}

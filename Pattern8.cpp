e37f2f1be394f2f91a62e8e1e6e1cd44589288d3
Vinayak Rodd <150567680+vinayakRodd@users.noreply.github.com>
2024-05-13 20:15:18 +0530

    Create Pattern8.cpp

---
+++ Pattern8.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace  std;
+
+
+void print1(int n){
+    
+    int stars=1,spaces;
+
+    for(int i=1; i<=2*n; i++){
+
+        spaces  = i;
+        
+        if(i <= n){
+            for(int k=n; k>=spaces; k--)
+                cout<<" ";
+                
+            for(int j=1; j<=stars; j++)
+                cout<<"* ";
+            
+            stars++;
+        }
+                
+        if(i>n){
+            
+            
+            for(int j=n; j<=spaces-1; j++)
+                cout<<" ";
+                
+                
+            for(int k=i; k<=2*n; k++)
+                cout<<"* ";
+               
+            
+            
+        }
+
+        
+
+        cout<<endl;
+        
+        
+    }
+
+}
+
+int main(){
+
+    int n;
+
+    cout<<"Enter the number of lines: ";
+    cin>>n;
+
+    print1(n);
+    
+
+
+
+}

f86b805b23bbe5cc4465d348166d04f25eeb6aee
Vinayak Rodd <150567680+vinayakRodd@users.noreply.github.com>
2024-05-15 11:19:13 +0530

    Create Pattern10.cpp

---
+++ Pattern10.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace  std;
+
+
+void print1(int n){
+    
+    int num;
+
+    for(int i=1; i<=n; i++){
+
+        num = 0;
+        
+        
+        for(int j=1; j<=2*n; j++){
+        
+            if(j<=i){
+                num++;
+                cout<<num;
+            }
+                    
+            else
+            if(j>i && j<=2*n-i){
+                cout<<" ";
+            }
+            
+            else{
+                cout<<num;
+                num--;
+            }
+            
+        }
+        cout<<endl;
+        
+        
+    }
+
+}
+
+int main(){
+
+    int n;
+
+    cout<<"Enter the number of lines: ";
+    cin>>n;
+
+    print1(n);
+    
+
+}
